4b1f9bd96878e3fd62c438e1c6c16a6dc9a7d543
kjoelovelife <34365220+kjoelovelife@users.noreply.github.com>
2018-09-13 18:21:51 +0800

    Update set_ros_master.sh

--- set_ros_master.sh
+++ set_ros_master.sh
@@ -3,11 +3,11 @@ echo "Setting ROS_MASTER_URI..."
 if [ $# -gt 0 ]; then
 	# provided a hostname, use it as ROS_MASTER_URI
 	export ROS_MASTER_URI=http://$1.local:11311/
-        export ROS_HOSTNAME=$HOSTNAME
 else
 	echo "No hostname provided. Using $HOSTNAME."
 	export ROS_MASTER_URI=http://$HOSTNAME.local:11311/
-        export ROS_HOSTNAME=$HOSTNAME
+
 fi
+export ROS_HOSTNAME=$HOSTNAME.local
 echo "ROS_MASTER_URI set to $ROS_MASTER_URI"
 echo "ROS_HOSTNAME set to $ROS_HOSTNAME"
