13779db473300c8a6a036daa1c0600634a58b76d
joe <joe@123.com>
2018-09-14 10:04:21 +0800

    modified openCR_setup.sh and add folder of opencr_update

--- openCR_setup.sh
+++ openCR_setup.sh
@@ -13,7 +13,4 @@ sudo udevadm trigger
 export OPENCR_PORT=/dev/ttyACM0
 export OPENCR_MODEL=burger
 rm -rf ./opencr_update.tar.bz2
-wget https://github.com/ROBOTIS-GIT/OpenCR/raw/develop/arduino/opencr_release/shell_update/opencr_update.tar.bz2 && tar -xvf opencr_update.tar.bz2 && cd ./opencr_update && ./update.sh $OPENCR_PORT $OPENCR_MODEL.opencr && cd ..
-
-rm -rf ./opencr_update.tar.bz2
-rm -rf ./opencr_update
+cd ./opencr_update && ./update.sh $OPENCR_PORT $OPENCR_MODEL.opencr && cd ..
