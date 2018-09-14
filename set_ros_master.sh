e841a7aeb3a9c209fc78f721c9896e56b34ff278
kjoelovelife <34365220+kjoelovelife@users.noreply.github.com>
2018-09-14 08:59:20 +0800

    Update set_ros_master.sh

--- set_ros_master.sh
+++ set_ros_master.sh
@@ -3,11 +3,12 @@ echo "Setting ROS_MASTER_URI..."
 if [ $# -gt 0 ]; then
 	# provided a hostname, use it as ROS_MASTER_URI
 	export ROS_MASTER_URI=http://$1.local:11311/
+	ip=C ifconfig | grep 'inet addr:' | grep -v '127.0.0.1' | cut -d:-f2 | awk '{print$2}'
 else
 	echo "No hostname provided. Using $HOSTNAME."
 	export ROS_MASTER_URI=http://$HOSTNAME.local:11311/
-
+        ip=C ifconfig | grep 'inet addr:' | grep -v '127.0.0.1' | cut -d:-f2 | awk '{print$1}'
 fi
-export ROS_HOSTNAME=$HOSTNAME.local
+export ROS_HOSTNAME=$ip
 echo "ROS_MASTER_URI set to $ROS_MASTER_URI"
-echo "ROS_HOSTNAME set to $ROS_HOSTNAME"
+echo "ROS_HOSTNAME set to $ip"
