5e3f8ac1c7a7a860e8e5051732e57761a60f7bda
kjoelovelife <34365220+kjoelovelife@users.noreply.github.com>
2018-09-15 22:23:22 +0800

    Update openCR_setup.sh

--- openCR_setup.sh
+++ openCR_setup.sh
@@ -1,4 +1,31 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Shell script scripts to upload driver to OpenCR 1.0 , and open permission openCR , rplidar.
+# -------------------------------------------------------------------------
+#Copyright © 2018 Wei-Chih Lin , kjoelovelife@gmail.com 
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+# -------------------------------------------------------------------------
+# This script is part of project Turtlebot3 , ROBOTIS 
+# Visit https://github.com/robotis-git/turtlebot3
+# Visit http://emanual.robotis.com/docs/en/platform/turtlebot3/getting_started/
+# -------------------------------------------------------------------------
 
 if [[ `id -u` -eq 0 ]] ; then
     echo "Do not run this with sudo (do not run random things with sudo!)." ;
